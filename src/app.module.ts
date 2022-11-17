bfa9747ad88cd42bd2f4a04093d98c84db3613db
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-16 23:35:07 -0300

    change DB to SQLite3, update app.module to this change, and config auth.service.ts to return Promise of the Usersentity

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,6 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: process.env.TYPEORM_CONNECTION,
-      host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
       synchronize: true,
